971ec1b797ecb76877d4d1650ba33d059b7fcf84
Ryan R1 Merchlewitz <ryan@totalexpert.com>
2020-07-15 09:43:36 -0500

    Got icons working by reverting to version 4

---
+++ src/components/NewItemPage.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'NewItemPage'
+    };
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
+import Ionic, { IonicVueRouter } from '@ionic/vue';
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+
 Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/^ion-/]
+
+Vue.use(Ionic);
+Vue.use(IonicVueRouter);
+
+const router = new IonicVueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/home',
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: () => import(/* webpackChunkName: 'home' */ '@/components/HomePage'),
+        },
+        {
+            path: '/new-item',
+            name: 'new-item',
+            component: () => import(/* webpackChunkName: 'new-item' */ '@/components/NewItemPage'),
+        },
+    ],
+});
 
 new Vue({
-  store,
-  render: (h) => h(App),
+    router,
+    store,
+    render: (h) => h(App),
 }).$mount('#app');
